a3221405c68da69fb03537a1b58d0742d38e68c7
mr567 <1627203545@qq.com>
2021-02-02 23:55:38 +0800

    解决Cart.php文件冲突并提交

--- application/common/model/Combination.php
+++ application/common/model/Combination.php
@@ -23,6 +23,8 @@ class Combination extends Model
     //自定义初始化
     protected static function init()
     {
+
+
         //TODO:自定义的初始化
     }
 
--- application/common/model/CombinationGoods.php
+++ application/common/model/CombinationGoods.php
@@ -23,6 +23,8 @@ class CombinationGoods extends Model
     //自定义初始化
     protected static function init()
     {
+
+
         //TODO:自定义的初始化
     }
 
